9464a2c793451da45dc203741700c3347c8602e2
Muhammad Munir <mmunir1214@gmail.com>
2024-08-22 19:13:42 -0400

    Finished POST methods

--- app/page.js
+++ app/page.js
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 import Image from "next/image";
 import { useState } from "react";
 
@@ -6,16 +6,48 @@ export default function Home() {
   const [messages, setMessages] = useState([
     {
       role: "assistant",
-      content: "Hello! I am the Rate My Professor support assistant. How can I help you today?",
+      content:
+        "Hello! I am the Rate My Professor support assistant. How can I help you today?",
     },
   ]);
 
-  const sendMessage = async => {
-    setMessage
+  const [message, setMessage] = useState("");
+  const sendMessage = (async) => {
+    setMessage((messages) => [
+      ...messages,
+      { role: "user", content: message },
+      { role: "assistant", content: "" },
+    ]);
+    setMessage("");
 
-  }
+    const response = fetch("/api/chat", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify([...messages, { role: "user", content: message }]),
+    }).then(async (res) => {
+      const reader = res.body.getReader();
+      const decoder = new TextDecoder();
 
-  return (
-    
-  )
+      let result = "";
+      return reader.read().then(function processText({ done, value }) {
+        if (done) {
+          return result;
+        }
+        const text = decoder.decode(value || new Uint8Array(), {
+          stream: !done,
+        });
+        setMessages((messages) => {
+          let lastMessage = messages[messages.length - 1];
+          let otherMessages = messages.slice(0, messages.length - 1);
+          return [
+            ...otherMessages,
+            { ...lastMessage, content: lastMessage.content + text },
+          ];
+        });
+        return reader.read().then(processText);
+      });
+    });
+  };
 }
